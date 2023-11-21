9468725f501b8377b7f368bc589eb9d5cf618d4a
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-21 03:28:19 -0500

    Remove need for thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,45 +1,63 @@
-use thiserror::Error;
-
 /// Types of errors that can occur when using SnowBinInfo, SnowBinWriter, and SnowBinReader.
-#[derive(Error, Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum SnowBinError {
     /// The data size used is not 8, 16, 32, or 64.
-    #[error("Data Size is not 8, 16, 32, or 64.")]
     DataSizeNotAllowed,
     /// The file given could not be created or opened.
-    #[error("Could not create or open the file.")]
     CouldNotCreateOrOpenFile,
     /// Could not write to the file for some reason.
-    #[error("Could not write to the file.")]
     IOWriteError,
     /// Header size given was < 8 bytes.
-    #[error("Header must be at least 8 bytes.")]
     HeaderSizeTooSmall,
     /// The header passed in was longer than the max header length allowed.
-    #[error("Header exceeds max header length.")]
     HeaderTooLong,
     /// Tried to write to file, but it was closed.
-    #[error("Could not write to the file because it was closed.")]
     IOWriterClosed,
     /// Data length exceeded the max data length allowed.
-    #[error("Data exceeds max length.")]
     DataTooLong,
     /// Could not read the file for some reason.
-    #[error("Could not read from the file.")]
     IOReadError,
     /// File did not start with the "SNOW_BIN" header.
-    #[error("File did not start with \"SNOW_BIN\" header.")]
     MalformedHeader,
     /// Tried to get a UInt from the file and failed.
-    #[error("Could not pull a uint from the file when expected.")]
     MalformedUInt,
     /// Spec version of the file did not match the program Spec version.
-    #[error("Spec version does not match.")]
     WrongSpecVersion,
     /// Reached the end of the file without finding the header.
-    #[error("Reached the end of the file, without finding the header specified.")]
     ReachedEOF,
     /// Verify hash does not match the data extracted.
-    #[error("Verification hash did not match data hash.")]
     HashDoesNotMatch,
 }
+impl std::fmt::Display for SnowBinError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SnowBinError::DataSizeNotAllowed => write!(f, "Data Size is not 8, 16, 32, or 64."),
+            SnowBinError::CouldNotCreateOrOpenFile => {
+                write!(f, "Could not create or open the file.")
+            }
+            SnowBinError::IOWriteError => write!(f, "Could not write to the file."),
+            SnowBinError::HeaderSizeTooSmall => write!(f, "Header must be at least 8 bytes."),
+            SnowBinError::HeaderTooLong => write!(f, "Header exceeds max header length."),
+            SnowBinError::IOWriterClosed => {
+                write!(f, "Could not write to the file because it was closed.")
+            }
+            SnowBinError::DataTooLong => write!(f, "Data exceeds max length."),
+            SnowBinError::IOReadError => write!(f, "Could not read from the file."),
+            SnowBinError::MalformedHeader => {
+                write!(f, "File did not start with \"SNOW_BIN\" header.")
+            }
+            SnowBinError::MalformedUInt => {
+                write!(f, "Could not pull a uint from the file when expected.")
+            }
+            SnowBinError::WrongSpecVersion => write!(f, "Spec version does not match."),
+            SnowBinError::ReachedEOF => write!(
+                f,
+                "Reached the end of the file, without finding the header specified."
+            ),
+            SnowBinError::HashDoesNotMatch => {
+                write!(f, "Verification hash did not match data hash.")
+            }
+        }
+    }
+}
+impl std::error::Error for SnowBinError {}
